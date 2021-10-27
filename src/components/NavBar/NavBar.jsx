379985cb0dede655f20f391d98e263a1f83bb334
Sujeesh VP <sujeeshkrishna98@gmail.com>
2021-10-27 09:09:38 +0530

    Created NavBar Title, and Icons

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Typography, Tooltip} from "@bigbinary/neetoui/v2";
+import { Search, Notification} from "@bigbinary/neeto-icons";
+
+function NavBar() {
+    
+    return (
+        <div className = "flex flex-row items-center w-360 h-16 left-0 top-0 bg-white shadow px-6 py-4">
+            <Typography style = "h4" className = "w-310 h- 6 left-6 top-5 flex-grow">Feed.ly</Typography>
+            <div className = "flex flex-row items-center 38 ml--9">
+                <Tooltip placement = {"bottom-end"} content = {"Search"} >
+                    <div>
+                        <Search/>  
+                    </div>
+                </Tooltip>
+                <Tooltip placement = {"bottom-end"} content = {"Subscribe"}>
+                    <div className = "mx-3">
+                        <Notification/>
+                    </div>
+                </Tooltip>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
