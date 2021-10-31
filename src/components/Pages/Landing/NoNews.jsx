03bae608d8b44e12362f4579ac6067a8b39c6fc2
Sujeesh VP <sujeeshkrishna98@gmail.com>
2021-10-31 19:53:31 +0530

    Created No Views page

--- src/components/Pages/Landing/NoNews.jsx
+++ src/components/Pages/Landing/NoNews.jsx
@@ -1,103 +1,53 @@
-import React from 'react'
-import Recommend from '../../Article/Recommend'
-import { Typography, Button} from "@bigbinary/neetoui/v2";
+import React, { useContext } from "react";
+import Recommend from "../../Article/Recommend";
+import { Typography, Button } from "@bigbinary/neetoui/v2";
+import { Highlight } from "@bigbinary/neeto-icons";
+import { globalDataContext } from "../../../App";
+
+
+
 function NoNews() {
-    
-    return (
-        <div>
-            <div className = "flex flex-col justify-center ">
-                <img src = "https://picsum.photos/200"/>
-                <Typography style="h6" weight="bold" className = "pt-1 pb-2 neeto-ui-text-gray-700"></Typography>
-                <Button label="Read More"  style="link" size = "small" />
-            </div>
-            <div className="flex flex-wrap pt-4 border-b-2 justify-between">
 
-                    <div className = "flex flex-row pb-4 w-2/5">
-                        <div className= "w-auto pr-4">
-                        <img src = "https://picsum.photos/200"/>
-                        </div>
-                        <div className= "flex flex-col">
-                            <Typography style="h6" weight="bold" className = "pt-1 pb-2 neeto-ui-text-gray-700">
-                            </Typography>
-                            <Typography  style="body3" weight="normal" className ="neeto-ui-text-gray-500 self-end pb-2">
-                            </Typography>
-                            <Button label="Read More"  style="link" size = "small" />
-                        </div>
-                    </div>
-                    <div className = "flex flex-row pb-4 w-2/5">
-                        <div className= "w-auto pr-4">
-                        <img src = "https://picsum.photos/200"/>
-                        </div>
-                        <div className= "flex flex-col">
-                            <Typography style="h6" weight="bold" className = "pt-1 pb-2 neeto-ui-text-gray-700">
-                            </Typography>
-                            <Typography  style="body3" weight="normal" className ="neeto-ui-text-gray-500 self-end pb-2">
-                            </Typography>
-                            <Button label="Read More"  style="link" size = "small" />
-                        </div>
-                    </div>
-                    <div className = "flex flex-row pb-4 w-2/5">
-                        <div className= "w-auto pr-4">
-                        <img src = "https://picsum.photos/200"/>
-                        </div>
-                        <div className= "flex flex-col">
-                            <Typography style="h6" weight="bold" className = "pt-1 pb-2 neeto-ui-text-gray-700">
-                            </Typography>
-                            <Typography  style="body3" weight="normal" className ="neeto-ui-text-gray-500 self-end pb-2">
-                            </Typography>
-                            <Button label="Read More"  style="link" size = "small" />
-                        </div>
-                    </div>
-                    <div className = "flex flex-row pb-4 w-2/5">
-                        <div className= "w-auto pr-4">
-                        <img src = "https://picsum.photos/200"/>
-                        </div>
-                        <div className= "flex flex-col">
-                            <Typography style="h6" weight="bold" className = "pt-1 pb-2 neeto-ui-text-gray-700">
-                            </Typography>
-                            <Typography  style="body3" weight="normal" className ="neeto-ui-text-gray-500 self-end pb-2"></Typography>yp
-                            <Button label="Read More"  style="link" size = "small" />
-                        </div>
-                    </div>
-                    <div className = "flex flex-row pb-4 w-2/5">
-                        <div className= "w-auto pr-4">
-                        <img src = "https://picsum.photos/200"/>
-                        </div>
-                        <div className= "flex flex-col">
-                            <Typography style="h6" weight="bold" className = "pt-1 pb-2 neeto-ui-text-gray-700">
-                            </Typography>
-                            <Typography  style="body3" weight="normal" className ="neeto-ui-text-gray-500 self-end pb-2">
-                            </Typography>
-                            <Button label="Read More"  style="link" size = "small" />
-                        </div>
-                    </div>
-                    <div className = "flex flex-row pb-4 w-2/5">
-                        <div className= "w-auto pr-4">
-                        <img src = "https://picsum.photos/200"/>
-                        </div>
-                        <div className= "flex flex-col">
-                            <Typography style="h6" weight="bold" className = "pt-1 pb-2 neeto-ui-text-gray-700">
-                            </Typography>
-                            <Typography  style="body3" weight="normal" className ="neeto-ui-text-gray-500 self-end pb-2">
-                            </Typography>
-                            <Button label="Read More"  style="link" size = "small" />
-                        </div>
-                    </div>
+const [globalData, setGlobalData] = useContext(globalDataContext);
+console.log("data....", globalData.randomList)
+
+  return (
+    <div>
+      <div className="flex flex-col justify-items-center h-50% pb-10 border-b-2">
+        <img src="/Images/NoNewsPage.png" className = "flex justify-center mx-auto w-1/5" />
+        <Typography
+          style="h3"
+          weight="semibold"
+          className="pt-1 pb-2 neeto-ui-text-gray-800 flex justify-center">No News Articles Found</Typography>
+        <Button icon = { Highlight} iconPosition="left" label="Write to us" style="secondary" size="small"  className = "flex justify-center w-28 mx-auto "/>
+      </div>
 
 
+      
+      
+      
+      
+      
+      <div className="flex flex-wrap pt-4 border-b-2 justify-between py-8">
 
-
-
-
-            
-            </div>   
-
-        </div>
-            
         
-    )
+        {globalData.randomList.map((item)=>{
+            return item.value.map((news,index)=> {
+                console.log("news..",item.key)
+                if(index <2){
+                   return  <Recommend key = {`newsContent${index}`} category={item.key} data={news} id={news.id}/>
+                }
+
+            })
+
+        })
+        
+        
+    
+        }
+      </div>
+    </div>
+  );
 }
 
-export default NoNews
-
-
+export default NoNews;
